c46a96464c070ffdc6daf096b75574f9b98c2854
pabloesmo <pabloesmo@hotmail.com>
2024-05-21 19:14:02 +0200

    add de un tester y avances

---
+++ TestServicio.java
@@ -0,0 +1,29 @@
+import es.upm.etsiinf.sos.*; 
+import es.upm.etsiinf.sos.model.xsd.*;
+
+public class TestServicio {
+	
+	public static void main(String[] args) {
+		WineSocialUPMSkeleton servicio = new WineSocialUPMSkeleton(); 
+		
+		User admin = new User();
+		admin.setName("admin");
+		admin.setPwd("admin");
+		
+		//simulamos que el admin ha iniciado sesion
+		servicio.usuarioActual = admin;
+		
+		// Prueba de AddUser
+        System.out.println("===== Añadir Usuario =====");
+        AddUser addUser = new AddUser();
+        Username username = new Username();
+       
+        username.setUsername("user1");
+        addUser.setArgs0(username);
+        
+        AddUserResponse addUserResponse = servicio.addUser(addUser);
+        System.out.println("Respuesta de añadir usuario: " + addUserResponse.get_return().getResponse());
+        
+        System.out.println("=======================");
+	}
+}
--- servicio/src/es/upm/etsiinf/sos/WineSocialUPMSkeleton.java
+++ servicio/src/es/upm/etsiinf/sos/WineSocialUPMSkeleton.java
@@ -8,8 +8,9 @@
 package es.upm.etsiinf.sos;
 import java.util.HashMap;
 import java.util.*;
-
 import es.upm.etsiinf.sos.model.xsd.User;
+import es.upm.etsiinf.sos.model.xsd.Username;
+import src.es.upm.fi.sos.t3.backend.es;
 /**
  * WineSocialUPMSkeleton java skeleton for the axisService
  */
@@ -197,22 +198,112 @@ public class WineSocialUPMSkeleton {
 		es.upm.fi.sos.t3.backend.RemoveUserResponse respuestaRemove = new es.upm.fi.sos.t3.backend.RemoveUserResponse();
 		es.upm.fi.sos.t3.backend.RemoveUser paramRemove = new es.upm.fi.sos.t3.backend.RemoveUser();
 		es.upm.fi.sos.t3.backend.xsd.RemoveUser removeDevuelto = new es.upm.fi.sos.t3.backend.xsd.RemoveUser();
-		es.upm.fi.sos.t3.backend.xsd removeModelo = new es.upm.fi.sos.t3.backend.xsd();
+		es.upm.etsiinf.sos.model.xsd.Response response = new es.upm.etsiinf.sos.model.xsd.Response();
+		es.upm.fi.sos.t3.backend.ExistUser usuarioExiste = new es.upm.fi.sos.t3.backend.ExistUser();
+		es.upm.fi.sos.t3.backend.ExistUserResponse respuestaUsuarioExiste = new es.upm.fi.sos.t3.backend.ExistUserResponse();
+		boolean eliminado = false;
 		
+		//obtengo el usuario y nombre del parametro de la funcion
 		Username usuario = removeUser.getArgs0();
 		String nombre_usuario = usuario.getUsername();
-		removeModelo.setName(nombre_usuario);
 		
-		//TODO: si el usuario existe dentro de mi memoria, entonces se puede borrar
-		//if(usuarios)
-		//solo el admin o el propio usuario pueden borrar
-		if(soyAdmin(usuarioActual)) {
-			
+		usuarioExiste.setUsername(nombre_usuario);
+		respuestaUsuarioExiste = auth.existUser(usuarioExiste);
+		boolean existe = respuestaUsuarioExiste.get_return().getResult();
+		
+		//obtengo el remove del backend y le pongo el nombre del usuario dado
+		removeDevuelto = paramRemove.getRemoveUser();
+		removeDevuelto.setName(nombre_usuario);
+		
+		//setteo ese remove del backend en el parametro del que le paso a la funcion de UPMAuth...
+		paramRemove.setRemoveUser(removeDevuelto);
+		
+		
+		//El admin NUNCA se puede borrar
+		if(nombre_usuario.equals("admin")) {
+			System.out.println("Hubo un error! No se puede borrar el usuario 'admin'\n");
+			response.setResponse(false);
+			respuestaFinalFuncion.set_return(response);
 		}
+		
+		//si el usuario no existe, obviamente NO se puede borrar
+		if(!existe) {
+			System.out.println("Hubo un error! No existe el usuario: '" + nombre_usuario +"'\n");
+			response.setResponse(false);
+			respuestaFinalFuncion.set_return(response);
+		}
+		
+		
+		//solo el admin o el propio usuario pueden borrar
+		if((soyAdmin(this.usuarioActual)) || (this.usuarioActual.getName().equals(nombre_usuario))) {
+			System.out.println("Si he entrado aqui, o soy el admin o soy el usuario que se quiere borrar...\n");
+			//llamo a la funcion y me devuelve una respuesta
+			respuestaRemove = auth.removeUser(paramRemove);
+			eliminado = respuestaRemove.get_return().getResult();
+			System.out.println("Se ha eliminado al usuario: '" + nombre_usuario + "'\n");
+			response.setResponse(eliminado);
+			respuestaFinalFuncion.set_return(response);
+		}
+		return respuestaFinalFuncion;
 	}
 	
 	
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param changePassword
+	 * @return changePasswordResponse
+	 */
+
+	public es.upm.etsiinf.sos.ChangePasswordResponse changePassword(es.upm.etsiinf.sos.ChangePassword changePassword) {
+		ChangePasswordResponse respuestaFinalFuncion = new ChangePasswordResponse();
+		es.upm.etsiinf.sos.model.xsd.Response response = new es.upm.etsiinf.sos.model.xsd.Response();
+		es.upm.fi.sos.t3.backend.ChangePasswordResponse respuestaAuth = new es.upm.fi.sos.t3.backend.ChangePasswordResponse();
+		es.upm.fi.sos.t3.backend.ChangePassword paramAuth = new es.upm.fi.sos.t3.backend.ChangePassword();
+		es.upm.etsiinf.sos.model.xsd.PasswordPair passw = new es.upm.etsiinf.sos.model.xsd.PasswordPair();
+		es.upm.fi.sos.t3.backend.xsd.ChangePasswordResponse respuestaBackend = new es.upm.fi.sos.t3.backend.xsd.ChangePasswordResponse();
+		
+		//extraigo la contraseñas del parametro
+		passw = changePassword.getArgs0();
+		//extraigo cada contraseña por si quisiese imprimirlas
+		String oldPassword = passw.getOldpwd();
+		String newPassword = passw.getNewpwd();
+		
+		//el cambio de contraseña del admin se trata de forma local
+		if(soyAdmin(this.usuarioActual) && admin.getPwd().equals(oldPassword)) {
+			System.out.println("Se ha cambiado la contraseña del admin correctamente!!");
+			admin.setPwd(newPassword);
+			response.setResponse(true);
+			respuestaFinalFuncion.set_return(response);
+		}
+		
+		paramAuth.setArgs0(passw);
+		
+		//llamo a la funcion del UPMAuth... y obtengo la respuesta
+		respuestaAuth = auth.changePassword(paramAuth);
+		respuestaBackend = respuestaAuth.get_return();
+		
+		
+		//si se ha cambiado correctamente
+		if(respuestaBackend.getResult()) {
+			System.out.println("Contraseña cambiada correctamente!!\n");
+			response.setResponse(true);
+			respuestaFinalFuncion.set_return(response);
+		}
+		return respuestaFinalFuncion;
+	}
 	
+	
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param addFollower
+	 * @return addFollowerResponse
+	 */
+
+	public es.upm.etsiinf.sos.AddFollowerResponse addFollower(es.upm.etsiinf.sos.AddFollower addFollower) {
+		
+	}
 	
 	
 	
@@ -229,19 +320,6 @@ public class WineSocialUPMSkeleton {
 				"Please implement " + this.getClass().getName() + "#getMyFollowers");
 	}
 
-
-	/**
-	 * Auto generated method signature
-	 * 
-	 * @param addFollower
-	 * @return addFollowerResponse
-	 */
-
-	public es.upm.etsiinf.sos.AddFollowerResponse addFollower(es.upm.etsiinf.sos.AddFollower addFollower) {
-		// TODO : fill this with the necessary business logic
-		throw new java.lang.UnsupportedOperationException(
-				"Please implement " + this.getClass().getName() + "#addFollower");
-	}
 
 	/**
 	 * Auto generated method signature
@@ -320,18 +398,6 @@ public class WineSocialUPMSkeleton {
 				"Please implement " + this.getClass().getName() + "#getWines");
 	}
 
-	/**
-	 * Auto generated method signature
-	 * 
-	 * @param changePassword
-	 * @return changePasswordResponse
-	 */
-
-	public es.upm.etsiinf.sos.ChangePasswordResponse changePassword(es.upm.etsiinf.sos.ChangePassword changePassword) {
-		// TODO : fill this with the necessary business logic
-		throw new java.lang.UnsupportedOperationException(
-				"Please implement " + this.getClass().getName() + "#changePassword");
-	}
 
 
 	/**
