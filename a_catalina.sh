6193e9c66c04441b180311527191a6d51c311aae
lsduser <lsduser@SOS-Restful.myguest.virtualbox.org>
2024-05-27 00:23:51 +0200

    a ver

--- a_catalina.sh
+++ a_catalina.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+echo "Ejecutamos ant"
+ant
+if [ $? -ne 0 ]; then
+  echo "Error al ejecutar ant"
+  exit 1
+fi
+echo "Ejecutado el ant en raiz"
+
 echo "Copiando..."
 cp build/lib/WineSocialUPM.aar $CATALINA_HOME/webapps/axis2/WEB-INF/services
+if [ $? -ne 0 ]; then
+  echo "Error al copiar el archivo"
+  exit 1
+fi
 echo "Copiado en catalina"
+
+echo "Reiniciamos el tomcat"
+$CATALINA_HOME/bin/shutdown.sh
+if [ $? -ne 0 ]; then
+  echo "Error al apagar Tomcat"
+  exit 1
+fi
+
+$CATALINA_HOME/bin/startup.sh
+if [ $? -ne 0 ]; then
+  echo "Error al iniciar Tomcat"
+  exit 1
+fi
+echo "Tomcat reiniciado correctamente"
