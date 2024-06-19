22eb490d92473dd09010caca0de4acb9bdfa3b77
pabloesmo <pabloesmo@hotmail.com>
2024-06-19 21:32:21 +0200

    superScript.sh creado

--- superscript.sh
+++ superscript.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-echo "Ejecutamos ant"
+# Borro lo que hay en la carpeta de Cliente
+ruta_carpeta="/Descargas/Cliente/src/es/upm/etsiinf/sos/"
+
+# Me voy a la carpeta de cliente para ejecutar el comando actualizarStub.sh
+cd /Descargas/Cliente
+
+# Ejecuto el script actualizarStub.sh
+echo "Actualizando stub..."
+$AXIS2_HOME/bin/wsdl2java.sh -uri WineSocialUPM.wsdl -wv 2.0 -p es.upm.etsiinf.sos -d adb
+
+# Me muevo a la carpeta Descargas
+cd ..
+
+# Ejecuto el script a_catalina.sh
+echo "Ejecuto ant en la raiz del proyecto"
+cd /Servicio
 ant
 if [ $? -ne 0 ]; then
   echo "Error al ejecutar ant"
   exit 1
 fi
-echo "Ejecutado el ant en raiz"
 
 echo "Copiando..."
 cp build/lib/WineSocialUPM.aar $CATALINA_HOME/webapps/axis2/WEB-INF/services
@@ -15,14 +29,6 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 echo "Copiado en catalina"
-
-echo "Ejecutando wsdl2java para actualizar el stub"
-$AXIS2_HOME/bin/wsdl2java.sh -uri WineSocialUPM.wsdl -wv 2.0 -p es.upm.etsiinf.sos -d adb
-if [ $? -ne 0 ]; then
-  echo "Error al ejecutar wsdl2java"
-  exit 1
-fi
-echo "Stub actualizado correctamente"
 
 echo "Reiniciamos el tomcat"
 $CATALINA_HOME/bin/shutdown.sh
@@ -37,3 +43,5 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 echo "Tomcat reiniciado correctamente"
+
+echo "Scripts ejecutados y contenido de la carpeta borrado exitosamente."
