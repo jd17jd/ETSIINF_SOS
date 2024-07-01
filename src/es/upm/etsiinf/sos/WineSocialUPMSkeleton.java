488ce26c8afbfe5e7d76b075a5f2946493b787b0
pabloesmo <pabloesmo@hotmail.com>
2024-07-01 17:52:57 +0200

    loggs y static

--- src/es/upm/etsiinf/sos/WineSocialUPMSkeleton.java
+++ src/es/upm/etsiinf/sos/WineSocialUPMSkeleton.java
@@ -36,7 +36,7 @@ public class WineSocialUPMSkeleton {
 
 	private boolean loggeado = false;
 	private static User admin;
-	private static User usuarioLoggeado;
+	private User usuarioLoggeado;
 	
 	public static Map<String,User> users;
 	public static Map<User,FollowerList> followersMap; // KEY: Nombre usuario -- VALUE: lista de seguidores
