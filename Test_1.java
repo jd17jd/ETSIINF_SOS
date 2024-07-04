c289963cd12776488a714b688991baa179d5f93e
jd17jd <juandieg01888@gmail.com>
2024-07-05 01:10:05 +0200

    prueba tests

--- Test_1.java
+++ Test_1.java
@@ -10,72 +10,24 @@ import es.upm.etsiinf.sos.WineSocialUPMStub.*;
 
 public class Test_1 {
 
-	/*
-	 * public static void Prueba_1(Cliente cliente) { String username = "admin";
-	 * String password = "admin"; boolean res = false;
-	 * 
-	 * try { res = cliente.login(username, password); if(res)
-	 * System.out.println("El admin se ha loggeado bien."); else
-	 * System.out.println("Error con el login");
-	 * 
-	 * res = cliente.changePassword(username, "adminNew"); if(res)
-	 * System.out.println("Admin ha cambiado la contraseña."); else
-	 * System.out.println("Error con el cambio de contraseña");
-	 * 
-	 * cliente.logout(); System.out.println("Usuario desloggeado");
-	 * 
-	 * res = cliente.login(username, "adminNew"); if(res)
-	 * System.out.println("El admin se ha loggeado de nuevo bien."); else
-	 * System.out.println("Error con el login");
-	 * 
-	 * } catch (RemoteException e) { e.printStackTrace(); } catch (Exception e) {
-	 * //Logout e.printStackTrace(); } }
-	 * 
-	 * public static void Prueba_2(Cliente cliente) { String username = "admin";
-	 * String password = "admin"; boolean res = false;
-	 * 
-	 * try { res = cliente.login(username, password); if(res)
-	 * System.out.println("El admin se ha loggeado bien."); else
-	 * System.out.println("Error con el login");
-	 * 
-	 * cliente.addUser("usuarioP");
-	 * System.out.println("Admin ha añadido a 'usuarioP'."); // else
-	 * System.out.println("Error al añadir usuario 'usuarioP'.");
-	 * 
-	 * cliente.addUser("usuarioP");
-	 * System.out.println("Admin ha añadido a 'usuarioP'."); // else
-	 * System.out.println("Error al añadir usuario 'usuarioP'");
-	 * 
-	 * } catch (RemoteException e) { e.printStackTrace(); } catch (Exception e) {
-	 * //Logout e.printStackTrace(); } }
-	 * 
-	 * public static void Prueba_6(Cliente cliente,Cliente cliente2) { boolean res =
-	 * false;
-	 * 
-	 * try { res = cliente.login("user2", "contraseñaMal"); if(res)
-	 * System.out.println("El user2 se ha loggeado bien."); else
-	 * System.out.println("Error con el login");
-	 * 
-	 * res = cliente.login("admin", "admin"); if(res)
-	 * System.out.println("El admin se ha loggeado bien."); else
-	 * System.out.println("Error con el login");
-	 * 
-	 * cliente.addUser("usuario2"); if(res)
-	 * System.out.println("Admin ha añadido a 'usuario2'."); else
-	 * System.out.println("Error al añadir usuario 'usuario2'");
-	 * 
-	 * 
-	 * } catch (RemoteException e) { e.printStackTrace(); } catch (Exception e) {
-	 * //Logout e.printStackTrace(); } }
-	 * 
-	 */
+	private static final String ADMINUSER = "admin";
+	private static final String ADMINPWD = "admin";
+	private static final String NEW_ADMINPWD = "admin3";
 
-	static WineSocialUPMStub admin;
-	static WineSocialUPMStub stub1;
-	static WineSocialUPMStub stub2;
-	static WineSocialUPMStub stub3;
+	private static final String USER1 = "testu1";
+
+
+	private static final String USER2 = "testu2";
+	private static String USER2PWD = "";
+
+
+	private static WineSocialUPMStub admin;
+	private static WineSocialUPMStub stub1;
+	private static WineSocialUPMStub stub2;
+	private static WineSocialUPMStub stub3;
+
 	static Logout logoutVacio = new Logout();
-	static RemoveUser userBorrar = new RemoveUser();
+	
 
 	private static void init() throws AxisFault {
 		admin = getStub();
@@ -98,7 +50,7 @@ public class Test_1 {
 		return stub;
 	}
 
-	private static void borradoUsuarios() {
+/* 	private static void borradoUsuarios() {
 		try {
 			UPMAuthenticationAuthorizationWSSkeletonStub st = new UPMAuthenticationAuthorizationWSSkeletonStub();
 			RemoveUserE removeUser0 = new RemoveUserE();
@@ -111,7 +63,7 @@ public class Test_1 {
 			var4.printStackTrace();
 		}
 
-	}
+	} */
 
 	private static Login createLoginData(String username, String password) {
 		Login res = new Login();
@@ -138,6 +90,18 @@ public class Test_1 {
 		res.setArgs0(param);
 		return res;
 	}
+
+	private static RemoveUser createRemoveUserData(String username) {
+		RemoveUser res = new RemoveUser();
+		Username param = new Username();
+		param.setUsername(username);
+		res.setArgs0(param);
+		return res;
+	}
+
+
+
+
 
 	private static AddFollower createFollowerData(String username) {
 		AddFollower addFriend1 = new AddFollower();
@@ -178,62 +142,145 @@ public class Test_1 {
 		return rateWine;
 	}
 
+	private static int test1() throws RemoteException {
+		System.out.println("********************** TEST 1 (value: 1)**********************");
+		System.out.println("1) admin login, 2) admin change password, 3) admin logout, 4) admin login con nueva pwd, 5) admin logout");
+		
+		admin = getStub();
+
+		//1
+		Login loginData = createLoginData(ADMINUSER, ADMINPWD);
+		boolean login = admin.login(loginData).get_return().getResponse();
+		System.out.println("Result admin login (exp true) : " + login);
+
+		//2
+		ChangePassword changePass = createChangePWDData(ADMINPWD, NEW_ADMINPWD);
+		boolean changePWD = admin.changePassword(changePass).get_return().getResponse();
+		System.out.println("Result admin changePwd (exp true) : " + changePWD);
+
+		//3
+		LogoutResponse response = admin.logout(logoutVacio);
+		boolean logout = response.get_return().getResponse();
+		System.out.println("Result admin logout (exp true) : " + logout);
+
+		//4
+		Login loginDataNew = createLoginData(ADMINUSER, NEW_ADMINPWD);
+		boolean loginNewPWD = admin.login(loginDataNew).get_return().getResponse();
+		System.out.println("Result admin login with new pwd (exp true) : " + loginNewPWD);
+
+		//5
+		LogoutResponse response2 = admin.logout(logoutVacio);
+		boolean logout2 = response2.get_return().getResponse();
+		System.out.println("Result admin logout (exp true) : " + logout2);
+
+		if (login && changePWD && logout && loginNewPWD && logout2) {
+			System.out.println("SUCCESS");
+			return 1;
+		}
+		else {
+			System.out.println("FAIL");
+			return 0;
+		}
+	}
+
+	private static int test2() throws RemoteException {
+		System.out.println("********************** TEST 2 (value: 1)**********************");
+		System.out.println("1) admin login, 2) admin addUser(testu1) x2 (segundo debe fallar), 3) admin logout");
+		
+		admin = getStub();
+		//1
+		Login loginData = createLoginData(ADMINUSER, ADMINPWD);
+		boolean login = admin.login(loginData).get_return().getResponse();
+		System.out.println("Result admin login (exp true) : " + login);
+
+		//2
+		AddUser addData1 = createAddData(USER1);
+		boolean adduser = admin.addUser(addData1).get_return().getResponse();
+		System.out.println("Result admin add user1 (exp true) : " + adduser);
+
+		AddUser addData2 = createAddData(USER1);
+		boolean adduserFail = admin.addUser(addData2).get_return().getResponse();
+		System.out.println("Result admin add user1 (exp false) : " + adduserFail);
+
+		//3
+		LogoutResponse response = admin.logout(logoutVacio);
+		boolean logout = response.get_return().getResponse();
+		System.out.println("Result admin logout (exp true) : " + logout);
+
+		if (login && adduser && !adduserFail && logout) {
+			System.out.println("SUCCESS");
+			return 1;
+		}
+		else {
+			System.out.println("FAIL");
+			return 0;
+		}
+	}
+
+
 	private static int test6() throws RemoteException {
-		try {
-			System.out.println("********************** TEST 6 (value: 1)**********************");
-			System.out.println(
-					"user2 login (falla), admin login, crea user2, user2 login otro st, remove user1 (falla), admin borra user1 (ok) borra user que no existe (falla)");
-			stub2 = getStub();
-			boolean loginFail = stub2.login(createLoginData(USER2, USER2PWD)).get_return().getResponse();
-			System.out.println("Result user login (exp false becuase user not registered) : " + loginFail);
-			admin = getStub();
-			boolean adminLogin = admin.login(createLoginData(ADMINUSER, ADMINPWD)).get_return().getResponse();
-			System.out.println("Result admin login (exp true) : " + adminLogin);
-			AddUserResponseE res = admin.addUser(createAddData(USER2));
-			boolean addUser2 = res.get_return().getResponse();
-			USER2PWD = res.get_return().getPwd();
-			System.out.println("Result add user2 (exp true) : " + addUser2);
-			boolean login = stub2.login(createLoginData(USER2, USER2PWD)).get_return().getResponse();
-			System.out.println("Result user2 login (exp true) : " + login);
-			Username username = new Username();
-			username.setUsername(USER1);
-			userBorrar.setArgs0(username);
-			boolean deleteFail = stub2.removeUser(userBorrar).get_return().getResponse();
-			System.out.println("Result user2 deletes user1 (exp false becuase not allowed) : " + deleteFail);
-			boolean delete = admin.removeUser(userBorrar).get_return().getResponse();
-			System.out.println("Result admin deletes user1 (exp true) : " + delete);
-			username.setUsername("user500");
-			userBorrar.setArgs0(username);
-			boolean deleteFailAdmin = admin.removeUser(userBorrar).get_return().getResponse();
-			System.out.println(
-					"Result admin deletes user500 (exp false bacause user500 didn't exist) : " + deleteFailAdmin);
-			stub2.logout(logoutVacio);
-			resetStub(stub2);
-			admin.logout(logoutVacio);
-			resetStub(admin);
-			System.out.println();
-			if (!loginFail && adminLogin && addUser2 && login && !deleteFail && delete && !deleteFailAdmin) {
-				System.out.println("SUCCESS");
-				return 1;
-			} else {
-				System.out.println("FAIL");
-				return 0;
-			}
-		} catch (NullPointerException var9) {
-			System.out.println();
-			System.out.println("FAIL - NULLPOINTER");
+		System.out.println("********************** TEST 6 (value: 1)**********************");
+		System.out.println("1) user2 login (falla), 2) admin login, 3) admin addsUser user2, 4) user2 login, 5) user2 remove user1 (falla), 6) admin remove user1, 7) admin remove user500 (falla)");
+
+		stub2 = getStub();
+
+		//1
+		Login loginData = createLoginData(USER2, USER2PWD);
+		boolean loginFail = stub2.login(loginData).get_return().getResponse();
+		System.out.println("Result user2 login (exp false becuase user not registered) : " + login);
+
+		admin = getStub();
+
+		//2
+		Login loginDataAdmin = createLoginData(ADMINUSER, ADMINPWD);
+		boolean loginAdmin = admin.login(loginDataAdmin).get_return().getResponse();
+		System.out.println("Result admin login (exp true) : " + loginAdmin);
+
+		//3
+		AddUser addData = createAddData(USER2);
+		AddUserResponse response = admin.addUser(addData);
+		boolean addUser = response.get_return().getResponse();
+		USER2PWD = response.get_return().getPwd();
+		System.out.println("Result add user2 (exp true) : " + addUser);
+
+		//4
+		Login loginData2 = createLoginData(USER2, USER2PWD);
+		boolean login = stub2.login(loginData2).get_return().getResponse();
+		System.out.println("Result user2 login (exp true) : " + login);
+
+		//5
+		RemoveUser removeUserData = createRemoveUserData(USER1);
+		boolean removeFail = stub2.removeUser(removeUserData).get_return().getResponse();
+		System.out.println("Result user2 deletes user1 (exp false becuase not allowed) : " + removeFail);
+
+		//6
+		boolean remove = admin.removeUser(removeUserData).get_return().getResponse();
+		System.out.println("Result admin deletes user1 (exp true) : " + remove);
+
+		//7
+		RemoveUser removeUserData2 = createRemoveUserData("user500");
+		boolean removeFailAdmin = admin.removeUser(removeUserData2).get_return().getResponse();
+		System.out.println("Result admin deletes user500 (exp false bacause user500 didn't exist) : " + removeFailAdmin);
+
+		if (!loginFail && loginAdmin && addUser && login && !removeFail && remove && !removeFailAdmin) {
+			System.out.println("SUCCESS");
+			return 1;
+		}
+		else {
+			System.out.println("FAIL");
 			return 0;
 		}
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("BORRADO USUARIOS...");
-		borradoUsuarios();
+		// System.out.println("BORRADO USUARIOS...");
+		// borradoUsuarios();
 
 		try {
 			init();
-		} catch (AxisFault var19) {
-			System.out.println("catch AxisFault with message: " + var19.getMessage());
+		}
+		catch (AxisFault e) {
+			System.out.println("catch AxisFault with message: " + e.getMessage());
 		}
 
 		double totalMark = 0.0D;
@@ -245,8 +292,8 @@ public class Test_1 {
 			arrayNotas[5] = nota;
 			totalMark += (double) nota;
 			notas = notas + nota + " / ";
-		} catch (RemoteException var16) {
-			System.out.println("catch RemoteException with message: " + var16.getMessage());
+		} catch (RemoteException e) {
+			System.out.println("catch RemoteException with message: " + e.getMessage());
 			System.out.println("FAILED");
 		}
 
