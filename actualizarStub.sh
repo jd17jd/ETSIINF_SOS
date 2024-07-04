6926f48cf0a390360adebe9c3f9364cad5c4edda
jd17jd <juandieg01888@gmail.com>
2024-07-04 15:59:47 +0200

    commit prueba 2 julio

--- actualizarStub.sh
+++ actualizarStub.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-echo "Borrando archivos..."
-
-rm -rf src/es/upm/etsiinf/sos/WineSocialUPMCallbackHandler.java
-rm -rf src/es/upm/etsiinf/sos/WineSocialUPMStub.java
-STATUS=$?
-
-# Comprobar el valor de salida
-if [ $STATUS -eq 0 ]; then
-	echo "Eliminación exitosa"
-else
-	echo "Error al eliminar"
-fi
-
-# Comando que ejecuta el wsdl2java para actualizar el stub
-
-echo "Generando archivos..."
+#comando que ejecuta el wsdl2java para actualizar el stub
 $AXIS2_HOME/bin/wsdl2java.sh -uri WineSocialUPM.wsdl -wv 2.0 -p es.upm.etsiinf.sos -d adb
