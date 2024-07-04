1935e5864d77ef11856a69feaf65afdae91ea27e
pabloesmo <pabloesmo@hotmail.com>
2024-07-04 17:21:09 +0200

    nuevos tests

--- Test_1.java
+++ Test_1.java
@@ -91,26 +91,6 @@ public class Test_1 {
 	
 	public static void main(String [] args) {
 		try {
-			Cliente clienteAdmin = new Cliente();
-			
-			System.out.println("PRUEBA_Nº1\n");
-			Prueba_1(clienteAdmin);
-			
-			System.out.println("PRUEBA_Nº2\n");
-			Prueba_2(clienteAdmin);
-			
-			System.out.println("PRUEBA_Nº6\n");
-//			Prueba_6(clienteAdmin);
-			
-		}
-		catch(Exception e) {
-			System.out.println("Fallo en stub del cliente\n"); 
-			e.printStackTrace();
-		}
-		
-		
-		
-		/* try {
 			
 			//stubs cada cliente ejecuta operaciones
 			Cliente clienteAdmin = new Cliente();
@@ -120,211 +100,157 @@ public class Test_1 {
 			Cliente cliente4 = new Cliente();
 			Cliente cliente5 = new Cliente();
 			
-			//TODO: Como sabemos la contraseña que se genera en el stub¿? 
-			//¿Ejecutamos una vez y vemos la contraseña (se guarda en backend) y luego podemos
-			//seguir ejecutando las demas pruebas? => Mejor opcion hacerlo secuencial.
-			
-			System.out.println("TEST Nº 1");
-			System.out.println("------------------\n");
-			
-			
-			if(cliente1.login("admin","admin")) {
-				System.out.println("El admin se ha loggeado bien.");
-			}
-			
-			if(cliente1.changePassword("admin","adminNew")) {
-				System.out.println("Admin ha cambiado la contraseña.");
-			}
-			
-			if(cliente1.logout()) {
-				System.out.println("Admin se desloggea.");
-			}
-			
-			if(cliente1.login("admin","adminNew")) {
-				System.out.println("El admin se ha loggeado de nuevo bien.");
-			}
-			
-			System.out.println("------------------\n");
-			
-			//////////////////////////////////////////////////////////////////////////////
-			//////////////////////////////////////////////////////////////////////////////
-
-			System.out.println("TEST Nº 2");
-			System.out.println("------------------\n");
-			
-			
-			if(cliente1.login("admin","admin")) {
-				System.out.println("El admin se ha loggeado.");
-			}
-			
-			if(cliente1.addUser("user")) {
-				System.out.println("El admin añade al usuario 'user'.");
-			}
-			
-			if(!(cliente1.addUser("user"))) {
-				System.out.println("El admin no debería haber añadido al usuario 'user' prq ya existe.");
-			}
-			
-			System.out.println("------------------\n");
-			
+			//user => usuarioP
+			//user2 => joselito
 			//////////////////////////////////////////////////////////////////////////////
 			//////////////////////////////////////////////////////////////////////////////
 			
 			System.out.println("TEST Nº 6");
 			System.out.println("------------------\n");
 
-			clienteAdmin.login("admin", "admin");
+			//se loggea user2 y da mal
+			boolean res = cliente1.login("joselito","noExiste");
+			if(res) System.out.println("El joselito se ha loggeado bien.");
+			else System.out.println("Error con el login");
 			
-			if(!(cliente2.login("user2","contraseñaDaIgual"))) {
-				System.out.println("Ha dado false prq no existe ese usuario.");
-			}
+			//se loggea admin
+			boolean res = cliente1.login("admin","admin");
+			if(res) System.out.println("El admin se ha loggeado bien.");
+			else System.out.println("Error con el login");
 			
-			if(cliente2.login("admin","admin")) {
-				System.out.println("El admin se ha loggeado bien.");
-			}
+			// Añadir un nuevo usuario
+	        AddUserResponse addUserResponse = cliente1.addUser("joselito");
+	        if (addUserResponse.getResponse()) {
+	        	System.out.println("Usuario joselito añadido correctamente.");
+	            System.out.println("Contraseña autogenerada: " + addUserResponse.getPassword());
+	        } else {
+	        	System.out.println("Error al añadir usuario joselito.");
+	        }
+	        
+	        
 			
-			if(cliente2.addUser("user2")) {
-				System.out.println("Admin ha creado el user2.");
-			}
-			
-			//como se que contraseña usar¿?
-			if(cliente3.login("user2","user2145")) {
-				System.out.println("Se loggea el user2 desde otro stub.");
-			}
-			
-			if(!(cliente2.removeUser("user1"))) {
-				System.out.println("No se ha borrado el user1 prq no tenia permisos.");
-			}
-			
-			if(clienteAdmin.removeUser("user1")) {
-				System.out.println("Admin ha borrado el user1.");
-			}
-			
-			if(!(clienteAdmin.removeUser("user500"))) {
-				System.out.println("No se ha borrado el user500 prq no existe.");
-			}
-			
+
 			System.out.println("------------------\n");
 			
 			//////////////////////////////////////////////////////////////////////////////	
 			//////////////////////////////////////////////////////////////////////////////
 						
-			System.out.println("TEST Nº 8");
-			System.out.println("------------------\n");
-			
-			
-			if(cliente1.login("user2","contraseñaDeUser2")) {
-				System.out.println("El user2 se ha loggeado bien en el stub1.");
-			}
-			
-			if(cliente2.login("user2","laMismaDeArriba")) {
-				System.out.println("El user2 se ha loggeado bien en el stub2.");
-			}
-			
-			if(cliente1.logout()) {
-				System.out.println("El user2 se ha desloggeado del stub1.");
-			}
-			
-			if(!(cliente1.changePassword("contraseñaDeAntes","nuevaContraseña"))) {
-				System.out.println("No se ha cambiado la contraseña en el stub1 prq hice logout.");
-			}
-			
-			if(!(cliente2.changePassword("contraseñaDeAntes","nuevaContraseña"))) {
-				System.out.println("Se ha cambiado la contraseña correctamente en el stub2.");
-			}
-			
-			System.out.println("------------------\n");
+//			System.out.println("TEST Nº 8");
+//			System.out.println("------------------\n");
+//			
+//			
+//			if(cliente1.login("user2","contraseñaDeUser2")) {
+//				System.out.println("El user2 se ha loggeado bien en el stub1.");
+//			}
+//			
+//			if(cliente2.login("user2","laMismaDeArriba")) {
+//				System.out.println("El user2 se ha loggeado bien en el stub2.");
+//			}
+//			
+//			if(cliente1.logout()) {
+//				System.out.println("El user2 se ha desloggeado del stub1.");
+//			}
+//			
+//			if(!(cliente1.changePassword("contraseñaDeAntes","nuevaContraseña"))) {
+//				System.out.println("No se ha cambiado la contraseña en el stub1 prq hice logout.");
+//			}
+//			
+//			if(!(cliente2.changePassword("contraseñaDeAntes","nuevaContraseña"))) {
+//				System.out.println("Se ha cambiado la contraseña correctamente en el stub2.");
+//			}
+//			
+//			System.out.println("------------------\n");
 			
 			//////////////////////////////////////////////////////////////////////////////
 			//////////////////////////////////////////////////////////////////////////////
 			
-			System.out.println("TEST Nº 10");
-			System.out.println("------------------\n");
-			
-			
-			if(cliente1.login("user2","contraseñaDeUser2")) {
-				System.out.println("El user2 se ha loggeado bien en el stub1.");
-			}
-			
-			if(cliente1.login("user2","contraseñaDeUser2")) {
-				System.out.println("El user2 se VUELVE a loggear en el stub1.");
-			}
-			
-			if(cliente1.logout()) {
-				System.out.println("El user2 se ha desloggeado del stub1.");
-			}
-			
-			if(!(cliente1.login("admin","admin"))) {
-				System.out.println("El admin no se pudo loggear en el stub1.");
-			}
-			
-			if(cliente1.logout()) {
-				System.out.println("El user2 se ha desloggeado del stub1.");
-			}
-			
-			if(cliente1.login("admin","admin")) {
-				System.out.println("El admin se ha loggeado en el stub1.");
-			}
-			
-			System.out.println("------------------\n");
+//			System.out.println("TEST Nº 10");
+//			System.out.println("------------------\n");
+//			
+//			
+//			if(cliente1.login("user2","contraseñaDeUser2")) {
+//				System.out.println("El user2 se ha loggeado bien en el stub1.");
+//			}
+//			
+//			if(cliente1.login("user2","contraseñaDeUser2")) {
+//				System.out.println("El user2 se VUELVE a loggear en el stub1.");
+//			}
+//			
+//			if(cliente1.logout()) {
+//				System.out.println("El user2 se ha desloggeado del stub1.");
+//			}
+//			
+//			if(!(cliente1.login("admin","admin"))) {
+//				System.out.println("El admin no se pudo loggear en el stub1.");
+//			}
+//			
+//			if(cliente1.logout()) {
+//				System.out.println("El user2 se ha desloggeado del stub1.");
+//			}
+//			
+//			if(cliente1.login("admin","admin")) {
+//				System.out.println("El admin se ha loggeado en el stub1.");
+//			}
+//			
+//			System.out.println("------------------\n");
 			
 			//////////////////////////////////////////////////////////////////////////////
 			//////////////////////////////////////////////////////////////////////////////
 
-			System.out.println("TEST Nº 11");
-			System.out.println("------------------\n");
-			
-			
-			if(cliente1.login("user2","contraseñaDeUser2")) {
-				System.out.println("El user2 se ha loggeado bien en el stub1.");
-			}
-			
-			if(cliente2.login("admin","admin")) {
-				System.out.println("El admin se loggea en el stub2.");
-			}
-			
-			if(cliente2.removeUser("user2")) {
-				System.out.println("El admin borra el user2.");
-			}
-			
-			if(cliente1.login("user2","contraseñaUser")) {
-				System.out.println("El user2 no se pudo loggear en el stub1.");
-			}
-						
-			System.out.println("------------------\n");
+//			System.out.println("TEST Nº 11");
+//			System.out.println("------------------\n");
+//			
+//			
+//			if(cliente1.login("user2","contraseñaDeUser2")) {
+//				System.out.println("El user2 se ha loggeado bien en el stub1.");
+//			}
+//			
+//			if(cliente2.login("admin","admin")) {
+//				System.out.println("El admin se loggea en el stub2.");
+//			}
+//			
+//			if(cliente2.removeUser("user2")) {
+//				System.out.println("El admin borra el user2.");
+//			}
+//			
+//			if(cliente1.login("user2","contraseñaUser")) {
+//				System.out.println("El user2 no se pudo loggear en el stub1.");
+//			}
+//						
+//			System.out.println("------------------\n");
 			
 			//////////////////////////////////////////////////////////////////////////////
 			//////////////////////////////////////////////////////////////////////////////
 			
-			System.out.println("TEST Nº 14");
-			System.out.println("------------------\n");
-			
-			
-			if(cliente2.login("admin","admin")) {
-				System.out.println("El admin se loggea en el stub2.");
-			}
-			
-			if(cliente2.addUser("user11")) {
-				System.out.println("El admin crea el 'user11'.");
-			}
-			
-			if(cliente1.login("user11","contraseñaUser11")) {
-				System.out.println("El user11 se loggea en el stub1.");
-			}
-			
-			if(cliente2.login("user11","contraseñaUser11")) {
-				System.out.println("El user11 se loggea en el stub2.");
-			}
-			
-			if(cliente1.removeUser("user11")) {
-				System.out.println("El user11 se borra a si mismo en stub1.");
-			}
-			
-			if(cliente2.login("user11","contraseñaUser11")) {
-				System.out.println("El user11 no se pudo loggear en el stub2.");
-			}
-						
-			System.out.println("------------------\n");
+//			System.out.println("TEST Nº 14");
+//			System.out.println("------------------\n");
+//			
+//			
+//			if(cliente2.login("admin","admin")) {
+//				System.out.println("El admin se loggea en el stub2.");
+//			}
+//			
+//			if(cliente2.addUser("user11")) {
+//				System.out.println("El admin crea el 'user11'.");
+//			}
+//			
+//			if(cliente1.login("user11","contraseñaUser11")) {
+//				System.out.println("El user11 se loggea en el stub1.");
+//			}
+//			
+//			if(cliente2.login("user11","contraseñaUser11")) {
+//				System.out.println("El user11 se loggea en el stub2.");
+//			}
+//			
+//			if(cliente1.removeUser("user11")) {
+//				System.out.println("El user11 se borra a si mismo en stub1.");
+//			}
+//			
+//			if(cliente2.login("user11","contraseñaUser11")) {
+//				System.out.println("El user11 no se pudo loggear en el stub2.");
+//			}
+//						
+//			System.out.println("------------------\n");
 			
 			
 		}
